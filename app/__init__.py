a76c237bee11a43a21979e8d438b5beb8165ebda
ValeskaLim <valeskavalentinekklesia@gmail.com>
2025-06-28 18:13:46 +0700

    add CORS for localhost

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app.config import Config
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app, origins="http://localhost:5173", supports_credentials=True, methods=['POST'])
     
     
     db.init_app(app)
