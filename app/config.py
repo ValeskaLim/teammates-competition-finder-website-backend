64596974718dc57aad1507f471d5d5f5671bc291
Valeska Valentin Ekklesia <106671548+ValeskaLim@users.noreply.github.com>
2025-07-20 02:26:58 +0700

    code formatting

--- app/config.py
+++ app/config.py
@@ -3,12 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
         f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
+
 def log_database_uri(self):
-        print(f"Database URI: {self.SQLALCHEMY_DATABASE_URI}")
+    print(f"Database URI: {self.SQLALCHEMY_DATABASE_URI}")
