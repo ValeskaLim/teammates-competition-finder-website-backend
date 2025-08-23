ee4db5f124edb8aecf77fd3431c96d9979c4734c
Valeska Valentin Ekklesia <106671548+ValeskaLim@users.noreply.github.com>
2025-08-24 04:14:22 +0700

    add docker setup

--- app/config.py
+++ app/config.py
@@ -5,6 +5,15 @@ load_dotenv()
 
 
 class Config:
+    # Get the environment (local or docker)
+    ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
+    
+    # Adjust DB_HOST based on environment
+    if ENVIRONMENT == "docker":
+        DB_HOST = "host.docker.internal"
+    else:
+        DB_HOST = os.getenv("DB_HOST", "localhost")
+    
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
         f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
